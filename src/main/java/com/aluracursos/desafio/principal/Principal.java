58715a0d6e448e43e1046800128fc306ea686e85
Boris <borisnandez93@gmail.com>
2024-11-20 19:50:36 -0600

    stream de libros

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio.principal;
 
 import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoApi;
 import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -15,5 +18,13 @@ public class Principal {
 
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados");
+        datos.libros().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
